274cfbfefec800e5164a6edb96416fa844718c5c
Kaiky Muniz <130181837+KaikyMuniz@users.noreply.github.com>
2023-10-15 23:38:44 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 document.getElementById("desativarBomba").addEventListener("click", function() {
     document.body.classList.add("ativar-escurecimento");
     setTimeout(function() {
-        window.location.href = "html/login.html";
+        window.location.href = "\u0068\u0074\u006D\u006C\u002F\u006C\u006F\u0067\u0069\u006E\u002E\u0068\u0074\u006D\u006C";
     }, 5000);
 });
 
 document.addEventListener('contextmenu', e => {
     e.preventDefault();
-  })  
+  })
