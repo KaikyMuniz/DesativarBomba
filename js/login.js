71b44fbc79f7afcf3b56b101a739573f23d2bef9
Kaiky Muniz <130181837+KaikyMuniz@users.noreply.github.com>
2023-10-15 23:28:59 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,4 +1,4 @@
-const text = "Seja bem vindo(a) ao project - Week Math -"; 
+const text = "Seja bem vindo(a) ao projeto name"; 
 const element = document.getElementById("typing-effect");
 
 element.textContent = "";
@@ -20,7 +20,7 @@ const btnSubmit = document.getElementById('submit');
 btnSubmit.addEventListener('click', function() {
     const username = document.getElementById('user').value;
     const username_correct = username.toUpperCase();
-    if (username_correct === "\x45\x55\x52\x45\x4b\x41") {
+    if (username_correct.trim() === "\x45\x55\x52\x45\x4b\x41") {
         window.location.href = "enigma.html";
     } else {
         window.alert('ERROR');
