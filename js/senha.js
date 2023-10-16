9b813b08d441c366a6fb33995186a0255ca32e2e
Kaiky Muniz <130181837+KaikyMuniz@users.noreply.github.com>
2023-10-15 23:29:55 -0300

    Update senha.js

--- js/senha.js
+++ js/senha.js
@@ -1,6 +1,8 @@
 const btnConfirm = document.getElementById('confirm');
 btnConfirm.addEventListener('click', function(){
-    if(document.getElementById('senha').value === "\u0034\u0031\u0033\u0032"){
+    const senha = document.getElementById('senha').value;
+    const senhaFormatada = senha.toLowerCase();
+    if(senhaFormatada.trim() === "\u006E\u0075\u006D\u0065\u0072\u006F\u0073\u0073\u0061\u006F\u0065\u0078\u0061\u0074\u006F\u0073"){
         window.location.href = "sucess.html";
     }else{
         window.alert("Senha incorreta");
